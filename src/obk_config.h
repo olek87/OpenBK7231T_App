b7d996c6a29ca043b448b79d8f17069fa608f434
olek87 <olek@walega.info>
2024-12-31 14:52:22 +0100

    Update obk_config.h
    enable i2c - #define ENABLE_I2C  1

--- src/obk_config.h
+++ src/obk_config.h
@@ -91,6 +91,7 @@
 #define ENABLE_TASMOTADEVICEGROUPS 1
 #define ENABLE_LITTLEFS			1
 #define ENABLE_NTP    1
+#define ENABLE_I2C    1
 //#define ENABLE_NTP_DST			1
 #define ENABLE_CALENDAR_EVENTS	1
 #define ENABLE_DRIVER_LED       1
